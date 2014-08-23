7643056f8d59bf8cc9ce6cb1c45c334a7f63637f
Jay Conrod <jayconrod@gmail.com>
2014-08-23 13:08:53 -0700

    VM: precompiled prefix header

---
+++ vm/src/prefix.h
@@ -0,0 +1,27 @@
+// Copyright 2014 Jay Conrod. All rights reserved.
+
+// This file is part of CodeSwitch. Use of this source code is governed by
+// the 3-clause BSD license that can be found in the LICENSE.txt file.
+
+
+// This header is intended to be precompiled. It doesn't define anything by itself. It just
+// includes a lot of other headers which are used by most files in the project.
+
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include "codeswitch.h"
+#include "array.h"
+#include "heap.h"
+#include "block-inl.h"
+#include "handle.h"
+#include "type.h"
+#include "utils.h"
+#include "vm.h"
