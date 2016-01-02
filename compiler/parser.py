22c249867d7a396e9e9822f60d9fb5d1861f18f6
Jay Conrod <jayconrod@gmail.com>
2016-01-01 20:41:23 -0500

    compiler: partial function expressions can no longer be used as normal expressions

--- compiler/parser.py
+++ compiler/parser.py
@@ -1,4 +1,4 @@
-# Copyright 2014-2015, Jay Conrod. All rights reserved.
+# Copyright 2014-2016, Jay Conrod. All rights reserved.
 #
 # This file is part of Gypsum. Use of this source code is governed by
 # the GPL license that can be found in the LICENSE.txt file.
@@ -414,7 +414,6 @@ def receiverExpr():
            whileExpr() | \
            breakExpr() | \
            continueExpr() | \
-           partialFnExpr() | \
            matchExpr() | \
            throwExpr() | \
            tryCatchExpr() | \
