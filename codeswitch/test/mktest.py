c0e96351a92bf7192e9a5a911c94270ea7f5239c
Jay Conrod <jayconrod@gmail.com>
2017-01-22 15:13:34 -0500

    rename compiler/ to gypsum/; vm/ to codeswitch/
    Remove remaining copyright notices

--- codeswitch/test/mktest.py
+++ codeswitch/test/mktest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2014-2016 Jay Conrod. All rights reserved.
+# Copyright Jay Conrod. All rights reserved.
 
 # This file is part of CodeSwitch. Use of this source code is governed by
 # the 3-clause BSD license that can be found in the LICENSE.txt file.
